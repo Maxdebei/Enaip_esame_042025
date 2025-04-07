e3eea84ff84351b613482b62dc05e67dc8cd85a7
Maxdebei <massimiliano.debei71@gmail.com>
2025-04-07 19:36:28 +0200

    ultima modifica

--- progetto/modulo2.py
+++ progetto/modulo2.py
@@ -8,7 +8,12 @@ def leggi_da_file(filepath):
     except FileNotFoundError:
         return None
 
-def processa_dati(dati):
+def processa_dati(dati: list) -> list:
+    lista_risultati = []
+    for diz in dati:
+        if diz["attivo"] == True:
+            lista_risultati.append(diz["id"])
+
     """
     Questa funzione dovrebbe processare una lista di dizionari.
     Implementa la logica per filtrare i dizionari che hanno una chiave 'attivo'
@@ -16,4 +21,8 @@ def processa_dati(dati):
     della chiave 'id' di questi dizionari.
     """
     # TODO: Implementare la logica di processamento dei dati
-    pass
+    return lista_risultati
+
+if __name__ == "__main__":
+    print (leggi_da_file("/home/max/progetti python/Enaip_esame_042025/dati/test.json"))
+
--- progetto/utils.py
+++ progetto/utils.py
@@ -16,4 +16,5 @@ def formatta_stringa(testo):
     con la prima lettera di ogni parola in maiuscolo.
     """
     # TODO: Implementare la formattazione della stringa
-    pass
+
+    return testo.title()
